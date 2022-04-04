c9337fb450fd041cb8047fdcc7e8980395f8dc18
paul <paul@qq.com>
2022-04-04 14:37:46 +0800

    添加石头剪布游戏

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # HelloRust
-Rust学习项目猜骰子游戏
+- Rust学习项目猜骰子游戏
+- Rust学习项目石头剪刀布
+## 运行
+```shell
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ use std::io;
 //     say(message.as_bytes(), width, &mut writer).unwrap();    
 // }
 
-fn main() {
+fn guess_number() {
     println!("猜数字 1-6");
     let secret_number = rand::thread_rng().gen_range(1..7);
     loop {
@@ -43,5 +43,73 @@ fn main() {
                 break;
             },
         }
+    }
+}
+
+fn rock_paper_shotgun() {
+    println!("开始石头剪刀布游戏!");
+
+    fn cmp(a:u32,b:u32) -> Ordering {
+        if (a == 1 && b == 3)||(a==2 && b== 1)||(a==3 && b== 2) {
+            return Ordering::Less
+        }else if (a == 1 && b == 2)||(a==2 && b== 3)||(a==3 && b== 1) {
+            return Ordering::Greater
+        }
+        
+        Ordering::Equal
+    }
+
+    fn get_char(c:u32) -> &'static str {
+        match c {
+            1 => "石头👊",
+            2 => "剪刀✌️",
+            3 => "布🖐",
+            _ => "Err",
+        }
+    }
+    loop {
+        println!("1 石头👊");
+        println!("2 剪刀✌️");
+        println!("3 布🖐");
+        
+        let game_number = rand::thread_rng().gen_range(1..4);
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("输入错误");
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{} 不是正确的数字.",guess.trim());
+                continue;
+            },
+        };
+
+        println!("你出的:{},机器人出:{}",get_char(guess),get_char(game_number));
+        match cmp(guess, game_number) {
+            Ordering::Less => println!("你输了！"),
+            Ordering::Equal => println!("不输不赢!"),
+            Ordering::Greater => {
+                println!("恭喜您赢了!");
+                break;
+            },
+        }
+    }
+}
+
+fn main() {
+    print!("1 猜数字游戏\n2 石头剪头布\n");
+    let mut game_type = String::new();
+    io::stdin().read_line(&mut game_type).expect("输入错误");
+    let start_type:u32 = match game_type.trim().parse() {
+        Ok(t) => t,
+        Err(_) => {
+            println!("{} 输入游戏类型不对.",game_type.trim());
+            0
+        },
+    };
+
+    if start_type == 1 {
+        guess_number();
+    }else if start_type == 2 {
+        rock_paper_shotgun();
     }
 }
